3841717dc91b061e9280cabb06741c64c63447a4
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2026-01-14 22:26:03 +0000

    Added commented out code for running as a script

--- PipelineDsl/Core/PipelineContext.cs
+++ PipelineDsl/Core/PipelineContext.cs
@@ -8,11 +8,8 @@ namespace PipelineDsl.Core;
 /// </summary>
 public class PipelineContext(ILogger logger, CiPlatform platform)
 {
-    private readonly Dictionary<string, object> _data = new();
-
     public ILogger Logger { get; } = logger;
     public CiPlatform Platform { get; } = platform;
-    public string WorkingDirectory { get; set; } = Environment.CurrentDirectory;
     public CancellationToken CancellationToken { get; set; } = CancellationToken.None;
 
     public static PipelineContext Create()
@@ -21,29 +18,4 @@ public class PipelineContext(ILogger logger, CiPlatform platform)
         var logger = LoggerFactory.Create(platform);
         return new PipelineContext(logger, platform);
     }
-
-    /// <summary>
-    /// Store arbitrary data in the context for sharing between steps.
-    /// </summary>
-    public void SetValue<T>(string key, T value) where T : notnull
-    {
-        _data[key] = value;
-    }
-
-    /// <summary>
-    /// Retrieve data from the context.
-    /// </summary>
-    public T? GetValue<T>(string key)
-    {
-        if (_data.TryGetValue(key, out var value) && value is T typedValue)
-        {
-            return typedValue;
-        }
-        return default;
-    }
-
-    /// <summary>
-    /// Check if a value exists in the context.
-    /// </summary>
-    public bool HasValue(string key) => _data.ContainsKey(key);
 }
